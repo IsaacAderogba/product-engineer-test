9c41309d9dc118463faa593c85750c96efc297d1
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-09-27 17:12:50 +0100

    Create reusable text styles

--- client/src/components/Cipher/CipherHeader.tsx
+++ client/src/components/Cipher/CipherHeader.tsx
@@ -1,10 +1,18 @@
 // modules
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+// components
+import pluralAILogo from "../../~reusables/assets/pluralai-logo.png";
 
 const CipherHeader = () => {
   return (
-    <div></div>
-  )
-}
+    <StyledCHeader>
+      <img src={pluralAILogo} alt="Plural AI logo" />
+    </StyledCHeader>
+  );
+};
 
-export default CipherHeader
+const StyledCHeader = styled.header``;
+
+export default CipherHeader;
---
+++ client/src/~reusables/atoms/Text.tsx
@@ -0,0 +1,122 @@
+// modules
+import styled from "styled-components";
+
+// styles
+import { text, titles } from "../variables/colors";
+import {
+  body_1,
+  body_2,
+  body_3,
+  heading_1,
+  heading_2,
+  heading_3,
+  heading_4,
+  heading_5,
+  heading_6
+} from "../variables/font-sizes";
+import { small_space } from "../variables/spacing";
+
+export interface TextProps {
+  color?: string;
+  mb?: string;
+  mt?: string;
+  ml?: string;
+  mr?: string;
+  align?: string;
+}
+
+export const Paragraph = styled("p")<TextProps>`
+  line-height: 1.6;
+  font-size: ${body_2};
+  color: ${(props: TextProps) => (props.color ? props.color : text)};
+  margin-bottom: ${(props: TextProps) => (props.mb ? props.mb : small_space)};
+  margin-top: ${(props: TextProps) => (props.mt ? props.mt : 0)};
+  margin-left: ${(props: TextProps) => (props.ml ? props.ml : 0)};
+  margin-right: ${(props: TextProps) => (props.mr ? props.mr : 0)};
+  text-align: ${(props: TextProps) => (props.align ? props.align : "left")};
+`;
+
+export const P1 = styled(Paragraph)`
+  font-size: ${body_1};
+`;
+
+export const P2 = styled(Paragraph)`
+  font-size: ${body_2};
+`;
+
+export const P3 = styled(Paragraph)`
+  font-size: ${body_3};
+`;
+
+export const H1 = styled("h1")<TextProps>`
+  line-height: 1.3;
+  font-size: ${heading_1};
+  font-weight: 700;
+  color: ${(props: TextProps) => (props.color ? props.color : titles)};
+  margin-bottom: ${(props: TextProps) => (props.mb ? props.mb : small_space)};
+  margin-top: ${(props: TextProps) => (props.mt ? props.mt : 0)};
+  margin-left: ${(props: TextProps) => (props.ml ? props.ml : 0)};
+  margin-right: ${(props: TextProps) => (props.mr ? props.mr : 0)};
+  text-align: ${(props: TextProps) => (props.align ? props.align : "left")};
+`;
+
+export const H2 = styled("h2")<TextProps>`
+  line-height: 1.3;
+  font-size: ${heading_2};
+  font-weight: 700;
+  color: ${(props: TextProps) => (props.color ? props.color : titles)};
+  margin-bottom: ${(props: TextProps) => (props.mb ? props.mb : small_space)};
+  margin-top: ${(props: TextProps) => (props.mt ? props.mt : 0)};
+  margin-left: ${(props: TextProps) => (props.ml ? props.ml : 0)};
+  margin-right: ${(props: TextProps) => (props.mr ? props.mr : 0)};
+  text-align: ${(props: TextProps) => (props.align ? props.align : "left")};
+`;
+
+export const H3 = styled("h3")<TextProps>`
+  line-height: 1.3;
+  font-size: ${heading_3};
+  font-weight: 700;
+  color: ${(props: TextProps) => (props.color ? props.color : titles)};
+  margin-bottom: ${(props: TextProps) => (props.mb ? props.mb : small_space)};
+  margin-top: ${(props: TextProps) => (props.mt ? props.mt : 0)};
+  margin-left: ${(props: TextProps) => (props.ml ? props.ml : 0)};
+  margin-right: ${(props: TextProps) => (props.mr ? props.mr : 0)};
+  text-align: ${(props: TextProps) => (props.align ? props.align : "left")};
+`;
+
+export const H4 = styled("h4")<TextProps>`
+  line-height: 1.4;
+  font-size: ${heading_4};
+  font-weight: 500;
+  color: ${(props: TextProps) => (props.color ? props.color : titles)};
+  margin-bottom: ${(props: TextProps) => (props.mb ? props.mb : small_space)};
+  margin-top: ${(props: TextProps) => (props.mt ? props.mt : 0)};
+  margin-left: ${(props: TextProps) => (props.ml ? props.ml : 0)};
+  margin-right: ${(props: TextProps) => (props.mr ? props.mr : 0)};
+  text-align: ${(props: TextProps) => (props.align ? props.align : "left")};
+`;
+
+export const H5 = styled("h5")<TextProps>`
+  line-height: 1.5;
+  font-size: ${heading_5};
+  font-weight: 500;
+  color: ${(props: TextProps) => (props.color ? props.color : titles)};
+  margin-bottom: ${(props: TextProps) => (props.mb ? props.mb : small_space)};
+  margin-top: ${(props: TextProps) => (props.mt ? props.mt : 0)};
+  margin-left: ${(props: TextProps) => (props.ml ? props.ml : 0)};
+  margin-right: ${(props: TextProps) => (props.mr ? props.mr : 0)};
+  text-align: ${(props: TextProps) => (props.align ? props.align : "left")};
+`;
+
+export const H6 = styled("h6")<TextProps>`
+  line-height: 1.5;
+  font-size: ${heading_6};
+  font-weight: 500;
+  color: ${(props: TextProps) => (props.color ? props.color : titles)};
+  margin-bottom: ${(props: TextProps) => (props.mb ? props.mb : small_space)};
+  margin-top: ${(props: TextProps) => (props.mt ? props.mt : 0)};
+  margin-left: ${(props: TextProps) => (props.ml ? props.ml : 0)};
+  margin-right: ${(props: TextProps) => (props.mr ? props.mr : 0)};
+  text-align: ${(props: TextProps) => (props.align ? props.align : "left")};
+  letter-spacing: 0.05em;
+`;
