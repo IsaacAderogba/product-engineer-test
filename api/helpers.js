0364fc4a67a5022242f7a11a9c149f9f3ad5126f
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-09-27 15:25:14 +0100

    Write implementation of substitution cipher

---
+++ api/helpers.js
@@ -0,0 +1,14 @@
+const subCipher = cipher => {
+  return cipher
+    .trim()
+    .split(" ")
+    .map(char => {
+      num = parseInt(char);
+      while (num >= 27) num = num / 27;
+      // adding 64 to num  will give the correct ascii value
+      // which will ultimately return the correct letter
+      num += 64;
+      return Number.isInteger(num) ? String.fromCharCode(num) : " ";
+    })
+    .join("");
+};
