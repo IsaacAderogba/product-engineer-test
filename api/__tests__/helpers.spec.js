1da4a565d418c050ede883aeb04308dfacc16b03
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-09-27 15:31:58 +0100

    Reuse cipher test to account for other coded messages

--- api/__tests__/helpers.spec.js
+++ api/__tests__/helpers.spec.js
@@ -4,5 +4,10 @@ describe("subCipher function", () => {
   it("returns the correct output for given range of inputs", () => {
     let output = subCipher("23 135 64 12 9 11 3645 64 18 19683 13 5 14");
     expect(output).toEqual("WE LIKE RAMEN");
-  });
+
+    output = subCipher("4374 243 14 20 5 59049 8");
+    expect(output).toEqual("FINTECH");
+
+    output = subCipher("16 12 567 486 1 12 64 27 243");
+    expect(output).toEqual("PLURAL AI");  });
 });
