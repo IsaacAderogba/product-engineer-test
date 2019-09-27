5ec18ba4151a9b6bf117f13c5ba005beb6e47ff5
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-09-27 16:50:57 +0100

    Write test to verify that App renders the Cipher component

--- client/src/components/__tests__/App.test.tsx
+++ client/src/components/__tests__/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+import { shallow } from "enzyme";
+import Cipher from "../Cipher/Cipher";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders the Cipher component", () => {
+  let wrapped = shallow(<App />);
+  expect(wrapped.find(Cipher).length).toEqual(1);
 });
