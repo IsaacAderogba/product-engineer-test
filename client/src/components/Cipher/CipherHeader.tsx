262d6be658d976c5ffe9f78b7913fe4847584524
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-09-27 17:30:53 +0100

    Add subtitle to CipherHeading

--- client/src/components/Cipher/CipherHeader.tsx
+++ client/src/components/Cipher/CipherHeader.tsx
@@ -4,44 +4,54 @@ import styled from "styled-components";
 
 // components
 import pluralAILogo from "../../~reusables/assets/pluralai-logo.png";
-import { H1 } from "../../~reusables/atoms/Text";
+import { H1, P1 } from "../../~reusables/atoms/Text";
 
 // styles
 import { offWhite } from "../../~reusables/variables/colors";
-import { xs_space, medium_space_2 } from "../../~reusables/variables/spacing";
+import { xs_space, medium_space_2, small_space } from "../../~reusables/variables/spacing";
 
 const CipherHeader = () => {
   return (
     <StyledCHeader>
-      <div>
-        <img src={pluralAILogo} alt="Plural AI logo" />
+      <div className="title">
+        <div>
+          <img src={pluralAILogo} alt="Plural AI logo" />
+        </div>
+        <H1 mb="0" color={offWhite}>
+          Plural AI Decoder
+        </H1>
       </div>
-      <H1 mb="0" color={offWhite}>
-        Plural AI Decoder
-      </H1>
+      <P1 mt={small_space} color={offWhite}>Input your code and we'll handle the rest</P1>
     </StyledCHeader>
   );
 };
 
 const StyledCHeader = styled.header`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding-top: ${medium_space_2};
+  padding: ${medium_space_2} 0;
+  
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  & > div {
-    height: 32px;
-    width: 32px;
-    margin-right: ${xs_space}
+    & > div {
+      height: 32px;
+      width: 32px;
+      margin-right: ${xs_space};
 
-    img {
-      height: 100%;
-      width: 100%:
+      img {
+        height: 100%;
+        width: 100%;
+      }
     }
-  }
 
-  h1 {
-    margin-left: ${xs_space}
+    h1 {
+      margin-left: ${xs_space};
+    }
   }
 `;
 
