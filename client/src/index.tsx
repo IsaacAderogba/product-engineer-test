71e596925b183d506d4ae949d6bb31f85aa2fc86
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-09-28 08:14:18 +0100

    Add ApolloProvider to index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,22 @@
+// modules
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import './index.css'
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// components
+import App from "./components/App";
+
+// styles
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
