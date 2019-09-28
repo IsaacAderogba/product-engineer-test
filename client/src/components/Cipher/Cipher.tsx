a9a6eed65f0ca3c3e4bb90f09372625d787bd8cd
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-09-28 08:01:05 +0100

    Create state for setting cipher input in Cipher

--- client/src/components/Cipher/Cipher.tsx
+++ client/src/components/Cipher/Cipher.tsx
@@ -1,23 +1,32 @@
 // modules
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useLazyQuery } from "@apollo/react-hooks";
 
-// components
+// components//functions
 import CipherInput from "./CipherInput";
 import DecodedCipher from "./DecodedCipher";
 import CipherHeader from "./CipherHeader";
 import CipherFooter from "./CipherFooter";
+import { decodeCipherQuery } from "../../queries/cipher";
 
 // styles
 import { primaryGradient } from "../../~reusables/variables/colors";
 
 const Cipher = () => {
+  const [cipherInput, setCipherInput] = useState("");
+  const [decodeCipher, { loading, data }] = useLazyQuery(decodeCipherQuery);
+
+  const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <StyledCipher>
       <div>
         <CipherHeader />
-        <form>
-          <CipherInput />
+        <form onSubmit={onSubmitForm}>
+          <CipherInput cipherInput={cipherInput} setCipherInput={setCipherInput} />
           <DecodedCipher />
         </form>
         <CipherFooter />
